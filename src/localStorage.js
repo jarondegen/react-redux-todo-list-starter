2418254e15f89d24e58d022c16ffa728f684dc84
Jaron Degen <jaron.degen90@gmail.com>
2020-09-22 22:24:51 +0200

    phase one complete

--- src/localStorage.js
+++ src/localStorage.js
@@ -1,6 +1,16 @@
 const STATE_KEY = 'tasks';
 
 export const loadState = () => {
+  try {
+    const stateJson = localStorage.getItem(STATE_KEY);
+    if (!state) {
+      return undefined;
+    }
+    const state = JSON.parse(stateJson);
+    return state;
+  }catch(err) {
+    console.warn(err);
+  }
   // TODO: Try to access the stored tasks state from local storage
   // TODO: If there is no state found, return `undefined`
   // TODO: If the state was found, return the state (parsed into JavaScript)
@@ -9,7 +19,13 @@ export const loadState = () => {
 };
 
 export const saveState = (state) => {
-  // TODO: Try to parse the state from JavaScript into a JSON string
+  try{
+    const stateJson = JSON.stringify(state);
+    localStorage.setItem(STATE_KEY, stateJson);
+  }catch(err) {
+    console.warn(err);
+  }
+    // TODO: Try to parse the state from JavaScript into a JSON string
   // TODO: Set the JSON string into local storage
 
   // TODO: Catch any errors with a `console.warn` statement
