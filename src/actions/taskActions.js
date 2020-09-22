17f61e1338b12602eb7d6ea01012221a1bdddb97
Andrew <travers.drew@gmail.com>
2020-09-22 16:45:41 -0400

    Completed Phase 2

--- src/actions/taskActions.js
+++ src/actions/taskActions.js
@@ -4,3 +4,21 @@
 // TODO: Define `createTask` action creator function
 
 // TODO: Define `deleteTask` action creator function
+
+export const CREATE_TASK = 'CREATE_TASK';
+export const DELETE_TASK = 'DELETE_TASK';
+
+export const createTask = (taskMessage) => {
+    return {
+        type: CREATE_TASK,
+        taskId: new Date().getTime(),
+        taskMessage,
+    }
+}
+
+export const deleteTask = (taskId) => {
+    return {
+        type: DELETE_TASK,
+        taskId,
+    }
+}
--- src/reducers/tasksReducer.js
+++ src/reducers/tasksReducer.js
@@ -1,6 +1,21 @@
 // TODO: Import `CREATE_TASK` and `DELETE_TASK` string literal constants
 
+import { CREATE_TASK, DELETE_TASK } from "../actions/taskActions";
+
 const tasksReducer = (state = {}, action) => {
+  Object.freeze(state);
+  const nextState = {...state };
+  switch (action.type) {
+    case CREATE_TASK:
+      const newTask = {id: new Date().getTime(), taskMessage: action.taskMessage}
+      nextState[action.taskId] = newTask;
+      return nextState;
+    case DELETE_TASK:
+      delete nextState[action.taskId];
+      return nextState;
+    default:
+      return state;
+  }
   // TODO: Freeze the state
   // TODO: Define switch case for routing 'CREATE_TASK' actions and 'DELETE_TASK' actions
 };
